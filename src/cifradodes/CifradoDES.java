2732bb8dd7e9d35bda559fd88c9c3cc58018b2bb
kirigalla <lrivasg1900@alumno.ipn.mx>
2021-10-05 20:36:47 -0500

    hola!

---
+++ src/cifradodes/CifradoDES.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cifradodes;
+
+/**
+ *
+ * @author crist
+ */
+public class CifradoDES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        cifrado ci=new cifrado();
+        ci.setVisible(true);
+    }
+    
+}
